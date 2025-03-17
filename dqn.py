0748d6b1324617a72c789107f84c1e0bd061f636
Daniel-Calvez <danielcalvez35@gmail.com>
2025-03-17 11:00:40 +0100

    ajout dqn vide

---
+++ dqn.py
@@ -0,0 +1,27 @@
+
+def convert_board():
+    return
+
+def read_CNN():
+    return
+
+def write_CNN():
+    return
+
+def cnn():
+    return
+
+def traduce_output():
+    return
+
+def filter_outputs():
+    return
+
+def random_select():
+    return
+
+def feedback():
+    return
+
+def loss():
+    return
