a2c878a0bc1f700493df4fa2e152f96b27b8ce98
Vincent Ducot <vducot@univ-rennes1.fr>
2025-03-18 11:44:21 +0100

    Debug convert tensor

--- dqn.py
+++ dqn.py
@@ -6,35 +6,43 @@ Daniel Calvez & Vincent Ducot
 '''
 
 import random
-import model as model
+import model 
 import numpy as np
 from icecream import ic
 
 def convert_board(board, player1, player2, active_player) -> np.array:
     tensor = np.zeros((6,6,7), dtype=np.int8)
 
-    for row in board:
-        for col in board[row]:
-            cell_content = board[row][col].strip()
-            prefix_p1 = player1[0:1]
-            prefix_p2 = player2[0:1]
+    for i, irow in enumerate(board):
+        for j, jcol in enumerate(irow):
+            cell_content = board[i][j].strip()
+            # ic(i)
+            # ic(j)
+            # ic(cell_content)
+            prefix_p1 = player1[0]+'_'
+            prefix_p2 = player2[0]+'_'
             if cell_content == prefix_p1+"X":  # X du joueur 1
-                tensor[row, col, 0] = 1
+                tensor[i,j, 0] = 1
             elif cell_content == prefix_p1+"O":  # O du joueur 1
-                tensor[row, col, 1] = 1
+                tensor[i,j, 1] = 1
             elif cell_content == prefix_p2+"X":  # X du joueur 2
-                tensor[row, col, 2] = 1
+                tensor[i,j, 2] = 1
             elif cell_content == prefix_p2+"O":  # O du joueur 2
-                tensor[row, col, 3] = 1
+                tensor[i,j, 3] = 1
             elif cell_content == "T_X":  # Totem X
-                tensor[row, col, 4] = 1
+                tensor[i,j, 4] = 1
             elif cell_content == "T_O":  # Totem O
-                tensor[row, col, 5] = 1
+                tensor[i,j, 5] = 1
             
             if active_player == player1:
-                tensor[row, col, 6] = 0
+                tensor[i,j, 6] = 2
             else:
-                tensor[row, col, 6] = 1
+                ic(active_player)
+                ic(player1)
+                tensor[i,j, 6] = 1
+    print(tensor[0,0,6])
+    ic(tensor[:,:,6])
+    ic(tensor)
     return tensor
 
 def read_CNN():
@@ -60,3 +68,14 @@ def feedback():
 
 def loss():
     return
+
+
+board = [
+    ['P_O', '   ', '   ', '   ', '   ', '   '],
+    ['   ', '   ', '   ', '   ', '   ', '   '],
+    ['   ', 'P_X', '   ', 'P_X', 'T_O', '   '],
+    ['   ', 'P_X', 'T_X', 'P_O', 'J_O', '   '],
+    ['   ', '   ', 'J_O', '   ', '   ', '   '],
+    ['   ', '   ', '   ', '   ', '   ', 'J_X']
+]
+tensor = convert_board(board, "Paul", "Jeanne", "Paul")
